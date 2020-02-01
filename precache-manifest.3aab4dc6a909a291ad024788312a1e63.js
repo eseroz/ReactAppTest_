2d97515e6a62f5791d340275922e2e89b7ab0353
Eser Öznohut <esero@outlook.com.tr>
2020-02-01 17:06:07 +0300

    Updates

---
+++ precache-manifest.3aab4dc6a909a291ad024788312a1e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aeab0babbcddfe9062b3eedfacb4457",
+    "url": "/ReactAppTest_/index.html"
+  },
+  {
+    "revision": "e80d59392ede79cb8159",
+    "url": "/ReactAppTest_/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2609e7bc21c494781b2c",
+    "url": "/ReactAppTest_/static/js/2.22fae775.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactAppTest_/static/js/2.22fae775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80d59392ede79cb8159",
+    "url": "/ReactAppTest_/static/js/main.fbb84761.chunk.js"
+  },
+  {
+    "revision": "c51eb2aa44777bfe7e33",
+    "url": "/ReactAppTest_/static/js/runtime-main.c9ec3bf6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactAppTest_/static/media/logo.5d5d9eef.svg"
+  }
+]);
