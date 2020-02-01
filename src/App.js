14eef43181dabe5c4e49fc96af78c9606aeb5b15
Eser Öznohut <esero@outlook.com.tr>
2020-02-01 17:21:31 +0300

    ddd

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
-        </a>
+          Ankara        </a>
       </header>
     </div>
   );
